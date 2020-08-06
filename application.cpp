c04b5d3c098012491d79047177b131ccde497664
s1270203 <t.kenji008@gmail.com>
2020-08-06 14:31:17 +0000

    Victory message added

--- application.cpp
+++ application.cpp
@@ -7,9 +7,11 @@ int main(){
     	std::mt19937 mt(rnd());
         std::uniform_int_distribution<> rand100(0,99);
 	cout << "Tossing a coin.." << endl;
+	int head = 0;
     	for (int i = 0; i < 3; ++i) {
 		cout << "Round " << i+1 << ":";
-        	if(rand100(mt)%2==0) cout << "Heads" << endl;
+        	if(rand100(mt)%2==0) cout << "Heads" << endl,head++;
         	else cout << "Tails" << endl;
     	};
+	cout << (3-head>head?"You lost":"You won") << endl;
 }
