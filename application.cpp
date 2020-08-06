82f2275bc4efd5c16a35fc6d77d96dc3fb0f27b7
s1270203 <t.kenji008@gmail.com>
2020-08-06 14:23:55 +0000

    HoT: Initial revision

---
+++ application.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <random>
+#include<iostream>
+using namespace std;
+int main(){
+	std::random_device rnd; 
+    	std::mt19937 mt(rnd());
+        std::uniform_int_distribution<> rand100(0,99);
+	cout << "Tossing a coin.." << endl;
+    	for (int i = 0; i < 3; ++i) {
+		cout << "Round " << i+1 << ":";
+        	if(rand100(mt)%2==0) cout << "Heads" << endl;
+        	else cout << "Tails" << endl;
+    	};
+}
